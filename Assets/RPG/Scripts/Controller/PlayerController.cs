10f3aff01fb730a1e53deb9cead44cda87fb0e60
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2022-10-26 22:58:40 +0100

    feat: FRPG-13 Introduce enemy type and debug log attack on click

--- Assets/RPG/Scripts/Controller/PlayerController.cs
+++ Assets/RPG/Scripts/Controller/PlayerController.cs
@@ -1,20 +1,33 @@
-using UnityEngine;
+using RPG.Combat;
 using RPG.Movement;
+using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace RPG.Controller
 {
     public class PlayerController : MonoBehaviour
     {
         private Mover MoveToTarget { get; set; }
+        private Fighter Fighter { get; set; }
 
         // Start is called before the first frame update
         void Start()
         {
             MoveToTarget = GetComponent<Mover>();
+            Fighter = GetComponent<Fighter>();
+
+            Assert.IsNotNull(Fighter);
+            Assert.IsNotNull(MoveToTarget);
         }
 
         // Update is called once per frame
         void Update()
+        {
+            HandleMovement();
+            HandleCombat();
+        }
+
+        private void HandleMovement()
         {
             if (Input.GetMouseButton(0))
             {
@@ -22,14 +35,34 @@ namespace RPG.Controller
             }
         }
 
+        private void HandleCombat()
+        {
+            RaycastHit[] raycastHits = Physics.RaycastAll(GetRayFromScreenPoint());
+
+            foreach (RaycastHit raycastHit in raycastHits)
+            {
+                Target target = raycastHit.transform.GetComponent<Target>();
+
+                if (target != null && Input.GetMouseButtonDown(0))
+                {
+                    Fighter.Attack(target);
+                }
+            }
+        }
+
         private void MoveToCursor()
         {
-            bool hasHit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit);
+            bool hasHit = Physics.Raycast(GetRayFromScreenPoint(), out RaycastHit hit);
 
             if (hasHit)
             {
                 MoveToTarget.MoveTo(hit.point);
             }
+        }
+
+        private Ray GetRayFromScreenPoint()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
