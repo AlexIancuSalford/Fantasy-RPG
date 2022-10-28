8bd936681a941229a89c7fd1c4f197f92a2980b2
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2022-10-28 03:26:16 +0100

    feat: FRPG-15 Player now moves to enemy within range to attack

--- Assets/RPG/Scripts/Combat/Fighter.cs
+++ Assets/RPG/Scripts/Combat/Fighter.cs
@@ -1,24 +1,43 @@
+using RPG.Movement;
 using UnityEngine;
 
 namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour
     {
+        private Transform TargetTransform { get; set; }
+        private Mover MoverRef { get; set; }
+
+        [field : SerializeField]
+        public float WeaponRange { get; set; }
+
         // Start is called before the first frame update
         void Start()
         {
-
+            MoverRef = GetComponent<Mover>();
         }
 
         // Update is called once per frame
         void Update()
         {
+            if (TargetTransform != null)
+            {
+                float distance = Vector3.Distance(TargetTransform.position, gameObject.transform.position);
 
+                if (distance >= WeaponRange)
+                {
+                    MoverRef.MoveTo(TargetTransform.position);
+                }
+                else
+                {
+                    MoverRef.Stop();
+                }
+            }
         }
 
         public void Attack(Target target)
         {
-            Debug.Log("Player Attacking " + target.name);
+            TargetTransform = target.transform;
         }
     }
 }
