45629ee196f4d7da1aacee148aba9407e5aa947f
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2022-12-17 05:49:33 +0000

    feat: FRPG-50 Now serialization is generic

--- Assets/RPG/Scripts/Helper/CSerializer.cs
+++ Assets/RPG/Scripts/Helper/CSerializer.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using UnityEngine;
 
@@ -21,43 +22,26 @@ namespace RPG.Helper
             return vectorBytes;
         }
 
-        public static Vector3 DeserializeVector(byte[] bufferBytes)
-        {
-            Vector3 vector = new Vector3
-            {
-                x = BitConverter.ToSingle(bufferBytes, 0),
-                y = BitConverter.ToSingle(bufferBytes, 4),
-                z = BitConverter.ToSingle(bufferBytes, 8)
-            };
-
-            return vector;
-        }
-
-        public static void WriteToFile(string fileName, byte[] bufferBytes)
+        public static void WriteToFile(string fileName, object obj)
         {
             string path = GetPathFromFile(fileName);
             Debug.Log("Saving to file " + path);
 
             using (FileStream fs = File.Open(path, FileMode.Create))
             {
-                fs.Write(bufferBytes, 0, bufferBytes.Length);
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(fs, obj);
             }
         }
 
-        public static void ReadFromFile(string fileName)
+        public static object ReadFromFile(string fileName)
         {
             string path = GetPathFromFile(fileName);
-            Debug.Log("Load from file " + path);
 
             using (FileStream fs = File.Open(path, FileMode.Open))
             {
-                byte[] buffer = new byte[fs.Length];
-                fs.Read(buffer, 0, buffer.Length);
-
-                Vector3 vector = DeserializeVector(buffer);
-                DeserializedVector = vector;
-
-                Debug.Log("Loaded from file: " + vector);
+                BinaryFormatter formatter = new BinaryFormatter();
+                return formatter.Deserialize(fs);
             }
         }
 
