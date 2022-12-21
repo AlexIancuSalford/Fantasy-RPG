8092a473fc255b6991a07dd5b66f9e965fc40171
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2022-12-21 17:32:32 +0000

    feat: FRPG-72 Create base stats class

---
+++ Assets/RPG/Scripts/Stats/BaseStats.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Stats
+{
+    public class BaseStats : MonoBehaviour
+    {
+        [Range(1, 99)]
+        [SerializeField] private int startingLevel = 1;
+        [field : SerializeField] private CharacterClass CharacterClass { get; set; }
+    }
+}
