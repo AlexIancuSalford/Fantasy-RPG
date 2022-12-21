d9ae69793b55ce3d0aa548d848fffa80092acab4
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2022-12-21 11:39:28 +0000

    feat: FRPG-69 Adjust projectile destroy time

--- Assets/RPG/Scripts/Combat/Projectile.cs
+++ Assets/RPG/Scripts/Combat/Projectile.cs
@@ -3,66 +3,71 @@ using System.Collections.Generic;
 using RPG.Core;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+namespace RPG.Combat
 {
-    private Health _target = null;
-    private float _damage = 0f;
-    private float _destroyTime = 3.0f;
-    
-    [field : SerializeField] private float Speed { get; set; }
-    [field : SerializeField] bool IsHoming { get; set; } = true;
-    [field : SerializeField] private GameObject HitEffect { get; set; } = null;
-
-    // Start is called before the first frame update
-    void Start()
+    public class Projectile : MonoBehaviour
     {
-        transform.LookAt(AimLocation());
-    }
+        private Health _target = null;
+        private float _damage = 0f;
+        private float _destroyTime = 3.0f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (_target == null) { return;}
+        [field: SerializeField] private float Speed { get; set; }
+        [field: SerializeField] bool IsHoming { get; set; } = true;
+        [field: SerializeField] private GameObject HitEffect { get; set; } = null;
 
-        if (IsHoming && !_target.IsDead) { transform.LookAt(AimLocation()); }
-        transform.Translate(Vector3.forward * Time.deltaTime * Speed);
-
-        // Cleanup
-        // Destroy all projectiles that missed their target
-        StartCoroutine(DelayDestroy(_destroyTime));
-    }
-
-    private Vector3 AimLocation()
-    {
-        CapsuleCollider targetCapsuleCollider = _target.GetComponent<CapsuleCollider>();
-        if (targetCapsuleCollider == null) { return _target.transform.position; }
-
-        return _target.transform.position + Vector3.up * targetCapsuleCollider.height / 2;
-    }
-
-    public void SetTarget(Health target, float damage)
-    {
-        _target = target;
-        _damage = damage;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.GetComponent<Health>() == null) { return; }
-        if (_target.IsDead) { return; }
-
-        if (HitEffect != null)
+        // Start is called before the first frame update
+        void Start()
         {
-            Instantiate(HitEffect, AimLocation(), transform.rotation);
+            transform.LookAt(AimLocation());
         }
 
-        _target.TakeDamage(_damage);
-        Destroy(gameObject);
-    }
+        // Update is called once per frame
+        void Update()
+        {
+            if (_target == null) { return; }
 
-    private IEnumerator DelayDestroy(float time)
-    {
-        yield return new WaitForSeconds(time);
-        Destroy(gameObject);
+            if (IsHoming && !_target.IsDead) { transform.LookAt(AimLocation()); }
+            transform.Translate(Vector3.forward * Time.deltaTime * Speed);
+
+            // Cleanup
+            // Destroy all projectiles that missed their target
+            StartCoroutine(DelayDestroy(_destroyTime));
+        }
+
+        private Vector3 AimLocation()
+        {
+            CapsuleCollider targetCapsuleCollider = _target.GetComponent<CapsuleCollider>();
+            if (targetCapsuleCollider == null) { return _target.transform.position; }
+
+            return _target.transform.position + Vector3.up * targetCapsuleCollider.height / 2;
+        }
+
+        public void SetTarget(Health target, float damage)
+        {
+            _target = target;
+            _damage = damage;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.GetComponent<Health>() == null) { return; }
+            if (_target.IsDead) { return; }
+
+            Speed = 0;
+
+            if (HitEffect != null)
+            {
+                Instantiate(HitEffect, AimLocation(), transform.rotation);
+            }
+
+            _target.TakeDamage(_damage);
+            Destroy(gameObject, 0.2f);
+        }
+
+        private IEnumerator DelayDestroy(float time)
+        {
+            yield return new WaitForSeconds(time);
+            Destroy(gameObject);
+        }
     }
 }
