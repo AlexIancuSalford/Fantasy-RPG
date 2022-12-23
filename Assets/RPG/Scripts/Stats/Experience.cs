4bdb72d9b42112287ce59090bf984dac6bdf5ee4
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2022-12-23 11:26:53 +0000

    feat: FRPG-82 Add event for levelling up

--- Assets/RPG/Scripts/Stats/Experience.cs
+++ Assets/RPG/Scripts/Stats/Experience.cs
@@ -1,3 +1,4 @@
+using System;
 using RPG.Save;
 using UnityEngine;
 
@@ -7,16 +8,33 @@ namespace RPG.Stats
     {
         [field : SerializeField] public float ExperiencePoints { get; private set; }
 
+        // Event that is called when the character gains experience points.
+        public event Action OnGainExperiencePoints;
+
+        /// <summary>
+        /// Adds the specified amount of experience points to the character's experience points.
+        /// </summary>
+        /// <param name="experiencePoints">The amount of experience points to add.</param>
         public void GainExperiencePoints(float experiencePoints)
         {
             ExperiencePoints += experiencePoints;
+            // Invoke the OnGainExperiencePoints event
+            OnGainExperiencePoints();
         }
 
+        /// <summary>
+        /// Saves the current state of the experience component.
+        /// </summary>
+        /// <returns>The current state of the experience component.</returns>
         public object SaveState()
         {
             return ExperiencePoints;
         }
 
+        /// <summary>
+        /// Loads the specified state of the experience component.
+        /// </summary>
+        /// <param name="obj">The state to load.</param>
         public void LoadState(object obj)
         {
             ExperiencePoints = (float)obj;
