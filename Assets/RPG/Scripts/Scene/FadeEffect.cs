ece1473328ce5c2205cc8836b68d274849e4dbb2
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2022-12-24 21:15:30 +0000

    fix: FRPG-95 Fix portal fade in and out bug

--- Assets/RPG/Scripts/Scene/FadeEffect.cs
+++ Assets/RPG/Scripts/Scene/FadeEffect.cs
@@ -6,6 +6,7 @@ namespace RPG.Scene
     public class FadeEffect : MonoBehaviour
     {
         private CanvasGroup _canvasGroup;
+        private Coroutine activeCoroutine = null;
 
         // Start is called before the first frame update
         void Awake()
@@ -13,30 +14,14 @@ namespace RPG.Scene
             _canvasGroup = GetComponent<CanvasGroup>();
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
         public IEnumerator FadeOut(float time)
         {
-            while (!_canvasGroup.alpha.Equals(1))
-            {
-                _canvasGroup.alpha += Time.deltaTime / time;
-
-                yield return null;
-            }
+            return Fade(1, time);
         }
 
         public IEnumerator FadeIn(float time)
         {
-            while (!_canvasGroup.alpha.Equals(0))
-            {
-                _canvasGroup.alpha -= Time.deltaTime / time;
-
-                yield return null;
-            }
+            return Fade(0, time);
         }
 
         private IEnumerator FadeOutAndIn()
@@ -49,6 +34,27 @@ namespace RPG.Scene
         public void FadeOutImmediately()
         {
             _canvasGroup.alpha = 1;
+        }
+
+        private IEnumerator FadeRoutine(float target, float time)
+        {
+            while (!Mathf.Approximately(_canvasGroup.alpha, target))
+            {
+                _canvasGroup.alpha = Mathf.MoveTowards(_canvasGroup.alpha, target, Time.deltaTime / time);
+
+                yield return null;
+            }
+        }
+
+        private IEnumerator Fade(float target, float time)
+        {
+            if (activeCoroutine != null)
+            {
+                StopCoroutine(activeCoroutine);
+            }
+
+            activeCoroutine = StartCoroutine(FadeRoutine(target, time));
+            yield return activeCoroutine;
         }
     }
 }
--- Assets/RPG/Scripts/Scene/Portal.cs
+++ Assets/RPG/Scripts/Scene/Portal.cs
@@ -1,57 +1,57 @@
-/*
- * This script is for a Portal object in a Unity game that is
- * used to transport the player character to another scene or location within
- * the game. When the player character collides with the Portal object
- * (as determined by the "OnTriggerEnter" function), the script starts a
- * coroutine called "LoadScene".
- *  
- * The "LoadScene" coroutine first finds an object in the scene with the
- * "FadeEffect" script, and uses it to fade out the screen. It then saves
- * the game state using a "SaveWrapper" object, loads the scene specified by
- * the "SceneToLoad" field, and loads the saved game state. It then finds the
- * other Portal object with the matching "DestinationPortal" field, and warps
- * the player character to the position and rotation of its child object
- * (which is likely meant to be the spawn point for the player character in
- * the destination scene). The script then waits for the specified
- * "FadeWaitTime" before fading the screen back in, and then destroys the
- * Portal game object.
- *
- * The script also includes several public fields that can be set in the Unity editor or via script:
- *  
- * "SceneToLoad": a string representing the name of the scene to load when
- * the player character enters the Portal.
- *
- * "DestinationPortal": an enumeration (enum) representing the destination
- * portal the player character will be transported to. The enum has eight
- * possible values: A, B, C, D, E, F, G, and H. The player character will be
- * teleported to the corresponding opposite portal (e.g. Postal A will take the
- * player character to the corresponding A portal in the selected scene) 
- *
- * "FadeOutTime": a float representing the time it takes for the screen to
- * fade out, in seconds.
- *
- * "FadeInTime": a float representing the time it takes for the screen to fade
- * in, in seconds.
- *
- * "FadeWaitTime": a float representing the time the script should wait before
- * fading the screen back in, in seconds.
- *
- * The script also includes two private fields:
- *  
- * "_spawnPoint": a Transform object representing the child object of the
- * Portal, which is likely the spawn point for the player character in the
- * destination scene.
- *
- * "saveWrapper": a SaveWrapper object used to save and load the game state.
- */
-
 using System.Collections;
+using RPG.Controller;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace RPG.Scene
 {
+    /// <summary>
+    /// This script is for a Portal object in a Unity game that is
+    /// used to transport the player character to another scene or location within
+    /// the game.When the player character collides with the Portal object
+    /// (as determined by the "OnTriggerEnter" function), the script starts a
+    /// coroutine called "LoadScene".
+    /// 
+    /// The "LoadScene" coroutine first finds an object in the scene with the
+    /// "FadeEffect" script, and uses it to fade out the screen.It then saves
+    /// the game state using a "SaveWrapper" object, loads the scene specified by
+    /// the "SceneToLoad" field, and loads the saved game state.It then finds the
+    /// other Portal object with the matching "DestinationPortal" field, and warps
+    /// the player character to the position and rotation of its child object
+    /// (which is likely meant to be the spawn point for the player character in
+    /// the destination scene). The script then waits for the specified
+    /// "FadeWaitTime" before fading the screen back in, and then destroys the
+    /// Portal game object.
+    /// 
+    /// The script also includes several public fields that can be set in the Unity editor or via script:
+    /// 
+    /// "SceneToLoad": a string representing the name of the scene to load when
+    /// the player character enters the Portal.
+    /// 
+    /// "DestinationPortal": an enumeration(enum) representing the destination
+    /// portal the player character will be transported to.The enum has eight
+    /// possible values: A, B, C, D, E, F, G, and H.The player character will be
+    /// teleported to the corresponding opposite portal (e.g.Postal A will take the
+    /// player character to the corresponding A portal in the selected scene)
+    /// 
+    /// "FadeOutTime": a float representing the time it takes for the screen to
+    ///  * fade out, in seconds.
+    /// 
+    /// "FadeInTime": a float representing the time it takes for the screen to fade
+    /// in, in seconds.
+    /// 
+    /// "FadeWaitTime": a float representing the time the script should wait before
+    /// fading the screen back in, in seconds.
+    /// 
+    /// The script also includes two private fields:
+    /// 
+    /// "_spawnPoint": a Transform object representing the child object of the
+    /// Portal, which is likely the spawn point for the player character in the
+    /// destination scene.
+    /// 
+    /// "saveWrapper": a SaveWrapper object used to save and load the game state.
+    ///  </summary>
     public class Portal : MonoBehaviour
     {
         public enum DestinationId
@@ -98,10 +98,21 @@ namespace RPG.Scene
             // Make the Portal object persist between scene loads
             DontDestroyOnLoad(gameObject);
 
+            // Grab the player and its PlayerController component so the player has the control disabled while the scene is loading
+            // I honestly don't know why it took me so long to find this obvious bug
+            PlayerController playerController = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+            playerController.enabled = false;
+
             // Fade out the screen
             yield return fadeEffect.FadeOut(FadeOutTime);
             saveWrapper.Save(); // Save the game state
             yield return SceneManager.LoadSceneAsync(SceneToLoad); // Load the new scene
+
+            // Grab the player and its PlayerController component so the player has the control disabled while the scene is loading
+            // This has to be done again as this is not the same player as the previous scene.
+            PlayerController playerControllerAgain = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+            playerControllerAgain.enabled = false;
+
             saveWrapper.Load(); // Load the saved game state
 
             // Find the other Portal object with the matching DestinationPortal field
@@ -120,6 +131,9 @@ namespace RPG.Scene
             yield return new WaitForSeconds(FadeWaitTime);
             // Fade the screen back in
             yield return fadeEffect.FadeIn(FadeInTime);
+
+            // Give control back to the player after the scene has finished loading
+            playerControllerAgain.enabled = true;
 
             // Destroy the Portal object
             Destroy(gameObject);
