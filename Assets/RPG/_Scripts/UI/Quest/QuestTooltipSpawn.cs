4e928fd36e81bbff98f02a5a6125a837fb37f95a
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2023-01-01 15:19:11 +0000

    feat: FRPG-156 Create and spawn tooltip for quest, also hide and show with button and key

---
+++ Assets/RPG/_Scripts/UI/Quest/QuestTooltipSpawn.cs
@@ -0,0 +1,18 @@
+using RPG.UI.Inventory;
+using UnityEngine;
+
+namespace RPG.UI.Quest
+{
+    public class QuestTooltipSpawn : TooltipSpawner
+    {
+        public override void UpdateTooltip(GameObject tooltip)
+        {
+            
+        }
+
+        public override bool CanCreateTooltip()
+        {
+            return true;
+        }
+    }
+}
