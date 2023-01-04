c0cbc6e734e7c7b5eaa33d6dddaad8542cdf78df
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2023-01-04 14:31:08 +0000

    feat: FRPG-157 Add new game menu and switcher between menus

---
+++ Assets/RPG/_Scripts/UI/Main Menu/MainMenuManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace RPG.UI.Menu
+{
+    public class MainMenuManager : MonoBehaviour
+    {
+        [field : SerializeField] public GameObject Entry { get; set; } = null;
+
+        private void Start()
+        {
+            SwitchTo(Entry);
+        }
+
+        public void SwitchTo(GameObject gameObject)
+        {
+            if (gameObject.transform.parent != transform) { return; }
+
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(child.gameObject == gameObject);
+            }
+        }
+    }
+}
